7fc6a5da1317ba3413ac1ccb721802369e98640b
Margaret LJ <margaretlj04@gmail.com>
2024-05-03 12:04:47 +0530

    initial commit

---
+++ edu/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Container} from 'reactstrap';
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <section>
+        <Container>
+            <div className='navigation'>
+                
+            </div>
+        </Container>
+    </section>
+  )
+}
